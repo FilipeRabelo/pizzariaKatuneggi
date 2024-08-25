b5bcd90d047bac6a3bffad347105165e2b522ec0
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-08-25 01:06:11 -0300

    atualizando BD

--- backend/src/prisma/index.ts
+++ backend/src/prisma/index.ts
@@ -1,5 +1,8 @@
+
+// instaciando a variavel como nova prisma client
+
 import { PrismaClient } from '@prisma/client';
 
-const prismaClient = new PrismaClient();  // instaciando a variavel como nova prisma client
+const prismaClient = new PrismaClient();
 
 export default prismaClient;
