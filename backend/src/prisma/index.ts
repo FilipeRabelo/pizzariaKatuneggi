38c7f9dfea136a8d3f2ea1221c51651401baebf8
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-08-24 23:53:44 -0300

    pizzaria katunegi

---
+++ backend/src/prisma/index.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from '@prisma/client';
+
+const prismaClient = new PrismaClient();  // instaciando a variavel como nova prisma client
+
+export default prismaClient;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,8 +5,9 @@ import cors from "cors";
 import { router } from './routes';
 
 const app = express();
+
 app.use(express.json());
-app.use(cors())     // yarn add @types/cors -D  para instalar as tipagem
+app.use(cors())           // yarn add @types/cors -D  cors para instalar as tipagem do typescript
 
 app.use(router);
 
@@ -14,21 +15,22 @@ app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => { // tipando a variavel err como Error //
 
-    if(err instanceof Error){  // se for uma instacia do tipo Error quero lancar uma execao
+  if(err instanceof Error){  // se for uma instacia do tipo Error quero lancar uma execao
 
-        return res.status(400).json({
-            error: err.message
-        })
-    }
-
-    return res.status(500).json({  // se nao for um error do tipo 400 vai ser um internalError
-        status: "error",
-        message: "Internal Server Error!!!"
+    return res.status(400).json({
+      error: err.message
     })
+  }
+
+  return res.status(500).json({  // se nao for um error do tipo 400 vai ser um internalError
+    status: "error",
+    message: "Internal Server Error!!!"
+  })
+
 })
+
 
 app.listen(3333, () =>
 
-    console.log("servidor online!!!! Bem-vindo Programador")
+  console.log("servidor online!!!! Bem-vindo Programador")
 );
-
