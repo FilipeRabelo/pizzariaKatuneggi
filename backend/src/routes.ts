68ff1c7b37e77d0fcf01482362e2469ccc9a121d
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-08-30 16:18:47 -0300

    Finalizando o backend da aplicação

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -22,9 +22,15 @@ import { RemoveOrderController } from "./controllers/order/RemoveOrderController
 
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
+import { SendOrderController } from "./controllers/order/SendOrderController";      // -- tirando pedido de rascunho - draft
+
+import { ListOrderController } from "./controllers/order/ListOrderController"       // -- listando os pedidos
+import { DetailOrderController } from "./controllers/order/DetailOrderController";  // -- listando os detalhes da order
+
+import { FinishOrderController } from "./controllers/order/FinishOrderController";  // -- fanalizar o pedido
 
 
-import { isAuthenticated } from './middlewares/isAuthenticated';   // -- autentificacao
+import { isAuthenticated } from './middlewares/isAuthenticated';                    // -- autentificacao
 
 
 import uploadConfig from './config/multer';
@@ -59,7 +65,15 @@ router.post('/order', isAuthenticated, new CreateOrderController().handle);   //
 router.delete('/orderDelete', isAuthenticated, new RemoveOrderController().handle);
 
 router.post('/order/add', isAuthenticated, new AddItemController().handle);  // -- post - cadastrar um novo item - novo item
-router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle); // -- delete item
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);   // -- delete item
+
+router.put('/order/send', isAuthenticated, new SendOrderController().handle) // -- PUT para atualizar - tirar o pedido de rascunho
+
+router.get('/orders', isAuthenticated, new ListOrderController().handle);   // -- get - para listar as order - pedidos
+
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle); // -- listando os itens do pedido
+
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle); // -- finalizano o pedido - alterando o status
 
 export { router };
 
